c3dc9a3668c966521532f2fd6dacbf955e8e7c87
Sergei <sergei.sustavov@cointelegraph.com>
2023-08-25 16:12:30 +0300

    updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
 
     /* Base URL to use in actions like `await page.goto('/')`. */
 
-    baseURL: 'https://cointelegraph.com',
+    baseURL: 'https://github.com/SergeSustavov',
     //navigationTimeout: 50 * 1000,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
